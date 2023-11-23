7dbaf2a6e028aaa23f38c4112f4cbc0171de317e
Supriya <145423817+supriyapeneti@users.noreply.github.com>
2023-11-23 14:47:14 +0530

    employe.py

--- employe.py
+++ employe.py
@@ -22,29 +22,38 @@ class Employee:
         overall_performance = (task_performance + attendance_percentage + leave_percentage) / 3
         return overall_performance
 
-# Streamlit web application with table and bar chart
+# Streamlit web application with table and bar chart for multiple employees
 def main():
     st.title("Employee Performance Calculator")
 
+    # Input for the number of employees
+    num_employees = st.number_input("Enter Number of Employees:", min_value=1, step=1)
+
+    # List to store Employee objects
+    employees = []
+
     # Input for employee details
-    name = st.text_input("Enter Employee Name:")
-    assigned_tasks = st.number_input("Enter Number of Assigned Tasks:", min_value=1, step=1)
-    total_attendance = st.number_input("Enter Total Attendance (in days):", min_value=0, max_value=30, step=1)
-    total_leaves = st.number_input("Enter Total Leaves (in days):", min_value=0, max_value=30, step=1)
+    for i in range(num_employees):
+        st.header(f"Employee {i + 1}")
+        name = st.text_input(f"Enter Employee {i + 1} Name:")
+        assigned_tasks = st.number_input(f"Enter Number of Assigned Tasks for Employee {i + 1}:", min_value=1, step=1)
+        total_attendance = st.number_input(f"Enter Total Attendance for Employee {i + 1} (in days):", min_value=0, max_value=30, step=1)
+        total_leaves = st.number_input(f"Enter Total Leaves for Employee {i + 1} (in days):", min_value=0, max_value=30, step=1)
 
-    # Create an instance of the Employee class
-    employee = Employee(name, assigned_tasks, total_attendance, total_leaves)
+        # Create an instance of the Employee class and add it to the list
+        employee = Employee(name, assigned_tasks, total_attendance, total_leaves)
+        employees.append(employee)
 
-    # Input for completed tasks
-    tasks_completed = st.number_input("Enter Number of Completed Tasks:", min_value=0, step=1)
-    employee.complete_task(tasks_completed)
+        # Input for completed tasks
+        tasks_completed = st.number_input(f"Enter Number of Completed Tasks for Employee {i + 1}:", min_value=0, step=1)
+        employee.complete_task(tasks_completed)
 
-    # Display performance
-    if assigned_tasks > 0:
+    # Display performance for each employee
+    for i, employee in enumerate(employees):
         performance_percentage = employee.calculate_performance()
-        st.write(f"Performance: {performance_percentage:.2f}%")
 
-        # Display table
+        # Display table for each employee
+        st.subheader(f"Employee {i + 1} Performance Details:")
         st.table({
             'Name': [employee.name],
             'Assigned Tasks': [employee.assigned_tasks],
@@ -54,12 +63,13 @@ def main():
             'Performance Percentage': [performance_percentage]
         })
 
-        # Bar chart
+        # Bar chart for each employee
         chart_data = {
             'Tasks Completed': employee.completed_tasks,
             'Attendance Percentage': (employee.total_attendance / 30) * 100,
             'Leaves Taken': employee.total_leaves
         }
+        st.subheader(f"Employee {i + 1} Performance Chart:")
         st.bar_chart(chart_data)
 
 if __name__ == "__main__":
