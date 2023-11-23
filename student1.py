436b3004567687201692c687584df0f76efa1648
Supriya <145423817+supriyapeneti@users.noreply.github.com>
2023-11-23 11:00:11 +0530

    student1.py

--- student1.py
+++ student1.py
@@ -1,16 +1,20 @@
 import streamlit as st
 
 class Employee:
-    def __init__(self, name, assigned_tasks):
+    def __init__(self, name, assigned_tasks, total_attendance):
         self.name = name
         self.assigned_tasks = assigned_tasks
         self.completed_tasks = 0
+        self.total_attendance = total_attendance
 
     def complete_task(self, tasks_completed):
         self.completed_tasks += tasks_completed
 
     def calculate_performance(self):
-        return (self.completed_tasks / self.assigned_tasks) * 100
+        task_performance = (self.completed_tasks / self.assigned_tasks) * 100
+        attendance_performance = (self.total_attendance / 30) * 100  # Assuming 30 working days in a month
+        overall_performance = (task_performance + attendance_performance) / 2
+        return overall_performance
 
 # Streamlit web application
 def main():
@@ -19,9 +23,10 @@ def main():
     # Input for employee details
     name = st.text_input("Enter Employee Name:")
     assigned_tasks = st.number_input("Enter Number of Assigned Tasks:", min_value=1, step=1)
+    total_attendance = st.number_input("Enter Total Attendance (in days):", min_value=0, max_value=30, step=1)
 
     # Create an instance of the Employee class
-    employee = Employee(name, assigned_tasks)
+    employee = Employee(name, assigned_tasks, total_attendance)
 
     # Input for completed tasks
     tasks_completed = st.number_input("Enter Number of Completed Tasks:", min_value=0, step=1)
