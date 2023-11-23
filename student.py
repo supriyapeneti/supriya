5f2e9bd1aaf8d360f8870cf2e8e74170b311e1c4
Supriya <145423817+supriyapeneti@users.noreply.github.com>
2023-11-23 10:41:52 +0530

    student.py

--- student.py
+++ student.py
@@ -1,37 +1,23 @@
 import streamlit as st
 
-# Base Class: Student
 class Student:
     def __init__(self, name, age, grade, address, contact_number, marks):
-        self._name = name
-        self._age = age
-        self._grade = grade
-        self._address = address
-        self._contact_number = contact_number
-        self._marks = marks
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.address = address
+        self.contact_number = contact_number
+        self.marks = marks
 
     def calculate_grade(self):
-        if self._marks > 90:
+        if self.marks > 90:
             return "A"
-        elif self._marks >= 80:
+        elif self.marks >= 80:
             return "B"
-        elif self._marks >= 70:
+        elif self.marks >= 70:
             return "C"
         else:
             return "D"
-
-    def display_details(self):
-        details = {
-            "Name": self._name,
-            "Age": self._age,
-            "Grade": self._grade,
-            "Address": self._address,
-            "Contact Number": self._contact_number,
-            "Marks": self._marks,
-            "Grade": self.calculate_grade()
-        }
-        return details
-
 
 # Streamlit UI
 def main():
@@ -49,14 +35,26 @@ def main():
         contact_number = st.text_input("Enter Contact Number:")
         marks = st.number_input("Enter Overall Marks:")
 
-        # Create an instance of StudentMarks
+        # Create an instance of Student
         student = Student(name, age, grade, address, contact_number, marks)
         students.append(student)
 
     # Display student details and marks in a table
-    details_list = [student.display_details() for student in students]
-    st.table(details_list)
+    details_list = []
+    for i, student in enumerate(students, 1):
+        details = {
+            "Student": i,
+            "Name": student.name,
+            "Age": student.age,
+            "Grade": student.grade,
+            "Address": student.address,
+            "Contact Number": student.contact_number,
+            "Marks": student.marks,
+            "Grade": student.calculate_grade()
+        }
+        details_list.append(details)
 
+    st.table(details_list)
 
 if __name__ == "__main__":
     main()
