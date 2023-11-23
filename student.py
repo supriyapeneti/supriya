3519225fc18b0a747cb1adb39b05148d2ca27d3e
Supriya <145423817+supriyapeneti@users.noreply.github.com>
2023-11-23 10:39:49 +0530

    student.py

--- student.py
+++ student.py
@@ -33,34 +33,29 @@ class Student:
         return details
 
 
-# Derived Class: StudentMarks (Inherits from Student)
-class StudentMarks(Student):
-    def __init__(self, name, age, grade, address, contact_number, marks):
-        super().__init__(name, age, grade, address, contact_number, marks)
-
-    def display_details(self):
-        details = super().display_details()
-        return details
-
-
 # Streamlit UI
 def main():
     st.title("Student Details and Marks")
 
-    # User input for common student details
-    name = st.text_input("Enter Student Name:")
-    age = st.number_input("Enter Student Age:")
-    grade = st.text_input("Enter Student Grade:")
-    address = st.text_area("Enter Address:")
-    contact_number = st.text_input("Enter Contact Number:")
-    marks = st.number_input("Enter Overall Marks:")
+    # Input details for multiple students
+    students = []
+    while st.button("Add Student"):
+        st.write(f"\n--- Student {len(students) + 1} ---")
+        # User input for common student details
+        name = st.text_input("Enter Student Name:")
+        age = st.number_input("Enter Student Age:")
+        grade = st.text_input("Enter Student Grade:")
+        address = st.text_area("Enter Address:")
+        contact_number = st.text_input("Enter Contact Number:")
+        marks = st.number_input("Enter Overall Marks:")
 
-    # Create an instance of StudentMarks
-    student = StudentMarks(name, age, grade, address, contact_number, marks)
+        # Create an instance of StudentMarks
+        student = Student(name, age, grade, address, contact_number, marks)
+        students.append(student)
 
     # Display student details and marks in a table
-    details = student.display_details()
-    st.table([details])
+    details_list = [student.display_details() for student in students]
+    st.table(details_list)
 
 
 if __name__ == "__main__":
