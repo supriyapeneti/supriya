3e55ee0d870adfa47e1eca663675b4e8218fefad
Supriya <145423817+supriyapeneti@users.noreply.github.com>
2023-11-23 10:57:05 +0530

    student1.py

--- student1.py
+++ student1.py
@@ -6,46 +6,31 @@ class Employee:
         self.assigned_tasks = assigned_tasks
         self.completed_tasks = 0
 
+    def complete_task(self, tasks_completed):
+        self.completed_tasks += tasks_completed
+
     def calculate_performance(self):
-        if self.assigned_tasks == 0:
-            return 0  # To avoid division by zero
         return (self.completed_tasks / self.assigned_tasks) * 100
 
-# Streamlit UI
+# Streamlit web application
 def main():
     st.title("Employee Performance Calculator")
 
-    # Input details for multiple employees
-    employees = []
-    while st.button("Add Employee"):
-        st.write(f"\n--- Employee {len(employees) + 1} ---")
-        # User input for employee details
-        name = st.text_input("Enter Employee Name:")
-        assigned_tasks = st.number_input("Enter Number of Assigned Tasks:", min_value=0, step=1)
+    # Input for employee details
+    name = st.text_input("Enter Employee Name:")
+    assigned_tasks = st.number_input("Enter Number of Assigned Tasks:", min_value=1, step=1)
 
-        # Create an instance of Employee
-        employee = Employee(name, assigned_tasks)
+    # Create an instance of the Employee class
+    employee = Employee(name, assigned_tasks)
 
-        # User input for completed tasks
-        completed_tasks = st.number_input("Enter Number of Completed Tasks:", min_value=0, step=1, max_value=employee.assigned_tasks)
-        employee.completed_tasks = completed_tasks
+    # Input for completed tasks
+    tasks_completed = st.number_input("Enter Number of Completed Tasks:", min_value=0, step=1)
+    employee.complete_task(tasks_completed)
 
-        employees.append(employee)
-
-    # Display employee performance in a table
-    performance_list = []
-    for i, employee in enumerate(employees, 1):
-        performance = {
-            "Employee": i,
-            "Name": employee.name,
-            "Assigned Tasks": employee.assigned_tasks,
-            "Completed Tasks": employee.completed_tasks,
-            "Performance (%)": employee.calculate_performance()
-        }
-        performance_list.append(performance)
-
-    st.table(performance_list)
+    # Display performance
+    if assigned_tasks > 0:
+        performance_percentage = employee.calculate_performance()
+        st.write(f"Performance: {performance_percentage:.2f}%")
 
 if __name__ == "__main__":
     main()
-
