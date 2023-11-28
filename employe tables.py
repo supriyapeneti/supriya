06b0b007300f1337de403349b4cae682034072e5
Supriya <145423817+supriyapeneti@users.noreply.github.com>
2023-11-28 09:20:27 +0530

    employe tables.py

--- employe tables.py
+++ employe tables.py
@@ -1,88 +1,135 @@
 import streamlit as st
-import pandas as pd
-from sqlalchemy import create_engine, Column, Integer, String, Float, MetaData, Table
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import psycopg2
 
-Base = declarative_base()
+# Define your PostgreSQL connection parameters
 
-class Employee(Base):
-    __tablename__ = 'employees'
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    assigned_tasks = Column(Integer)
-    completed_tasks = Column(Integer)
-    total_attendance = Column(Integer)
-    total_leaves = Column(Integer)
+# Create a connection to the PostgreSQL database
+conn = psycopg2.connect(
+    database="employee", user='user', password='5001', host='127.0.0.1', port='5432'
+)
+cursor = conn.cursor()
 
-# Database setup
-DATABASE_URL = "sqlite:///.desktop/employee_performance.db"
-engine = create_engine(DATABASE_URL)
-Base.metadata.create_all(bind=engine)
+# Create a table to store employee information if it doesn't exist
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS employees (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    attendance INTEGER NOT NULL,
+    productivity INTEGER NOT NULL,
+    teamwork INTEGER NOT NULL,
+    leave INTEGER NOT NULL,
+    feedback TEXT,
+    goals_achieved INTEGER NOT NULL,
+    self_assessment INTEGER NOT NULL,
+    performance_score INTEGER NOT NULL
+);
+'''
+cursor.execute(create_table_query)
+conn.commit()
 
-# Streamlit web application with individual performance tables
-def main():
-    st.title("Employee Performance Calculator")
+# Added the sidebar
+app_mode = st.sidebar.radio(
+    "",
+    ("Employee Perfomance Calculator", "Retrive data by Employee name"),
+)
 
-    # Input for multiple employees
-    num_employees = st.number_input("Enter Number of Employees:", min_value=1, step=1)
+if app_mode == "Employee Perfomance Calculator":
 
-    employees = []
-    for i in range(num_employees):
-        name = st.text_input(f"Enter Employee {i+1} Name:")
-        assigned_tasks = st.number_input(f"Enter Number of Assigned Tasks for Employee {i+1}:", min_value=1, step=1)
-        total_attendance = st.number_input(f"Enter Total Attendance (in days) for Employee {i+1}:", min_value=0, max_value=30, step=1)
-        total_leaves = st.number_input(f"Enter Total Leaves (in days) for Employee {i+1}:", min_value=0, max_value=30, step=1)
+    class Employee:
+        def __init__(self, name, attendance, productivity, teamwork, leave, feedback=None, goals_achieved=0,
+                     self_assessment=0):
+            self.name = name
+            self.attendance = attendance
+            self.productivity = productivity
+            self.teamwork = teamwork
+            self.leave = leave
+            self.feedback = feedback if feedback else "No feedback provided"
+            self.goals_achieved = goals_achieved
+            self.self_assessment = self_assessment
+            self.performance_score = None  # Initialize performance_score
 
-        employee = Employee(name=name, assigned_tasks=assigned_tasks, total_attendance=total_attendance, total_leaves=total_leaves)
-        employees.append(employee)
+        def calculate_performance(self):
+            # Define weights for each criterion
+            attendance_weight = 0.2
+            productivity_weight = 0.4
+            teamwork_weight = 0.2
+            leave_weight = 0.1
+            goals_weight = 0.2
+            self_assessment_weight = 0.1
 
-    # Display individual performance for each employee
-    for i, employee in enumerate(employees):
-        st.write(f"\n**Employee {i+1} - {employee.name}**")
+            # Calculate overall performance score
+            self.performance_score = (
+                    self.attendance * attendance_weight +
+                    self.productivity * productivity_weight +
+                    self.teamwork * teamwork_weight -
+                    self.leave * leave_weight +
+                    self.goals_achieved * goals_weight +
+                    self.self_assessment * self_assessment_weight
+            )
 
-        # Task Performance
-        st.subheader("Task Performance:")
-        task_performance_data = {
-            'Metric': ['Completed Tasks', 'Assigned Tasks', 'Task Performance Percentage'],
-            'Value': [0, employee.assigned_tasks, 0]
-        }
-        st.table(task_performance_data)
+            return self.performance_score
 
-        # Attendance Performance
-        st.subheader("Attendance Performance:")
-        attendance_performance_data = {
-            'Metric': ['Total Attendance Days', 'Attendance Percentage'],
-            'Value': [employee.total_attendance, 0]
-        }
-        st.table(attendance_performance_data)
 
-        # Leave Performance
-        st.subheader("Leave Performance:")
-        leave_performance_data = {
-            'Metric': ['Total Leaves Taken', 'Leave Performance Percentage'],
-            'Value': [employee.total_leaves, 0]
-        }
-        st.table(leave_performance_data)
+    # performance_app.py
 
-        # Overall Performance
-        st.subheader("Overall Performance:")
-        overall_performance_data = {
-            'Metric': ['Overall Performance Percentage'],
-            'Value': [0]
-        }
-        st.table(overall_performance_data)
+    def insert_employee_data(employee):
+        # Insert employee data into the database
+        insert_query = '''
+            INSERT INTO employees (name, attendance, productivity, teamwork, leave, feedback, goals_achieved, self_assessment, performance_score)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            '''
+        cursor.execute(insert_query, (employee.name, employee.attendance, employee.productivity, employee.teamwork,
+                                      employee.leave, employee.feedback, employee.goals_achieved,
+                                      employee.self_assessment,
+                                      employee.calculate_performance()))  # Use calculate_performance() here
+        conn.commit()
 
-        # Store employee data in the database
-        Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-        session = Session()
-        session.add(employee)
-        session.commit()
-        session.close()
 
-    # Save data to Excel file
-    df = pd.DataFrame([employee.__dict__ for employee in employees])
-    df.to_excel("employee_data.xlsx", index=False)
+    def main():
+        st.title("Employee Performance Calculator")
+
+        # Get employee information from user input
+        employee_name = st.text_input("Enter employee name:")
+        attendance = st.slider("Attendance:", 0, 100, 50)
+        productivity = st.slider("Productivity:", 0, 100, 50)
+        teamwork = st.slider("Teamwork:", 0, 100, 50)
+        leave = st.slider("Leave taken (days):", 0, 30, 0)
+        feedback = st.text_area("Feedback:")
+        goals_achieved = st.number_input("Number of goals achieved:", 0, 10, 0)
+        self_assessment = st.slider("Self-assessment:", 0, 100, 50)
+
+        # Create Employee instance
+        employee = Employee(
+            name=employee_name,
+            attendance=attendance,
+            productivity=productivity,
+            teamwork=teamwork,
+            leave=leave,
+            feedback=feedback,
+            goals_achieved=goals_achieved,
+            self_assessment=self_assessment,
+        )
+
+        # Calculate button
+        if st.button("Calculate"):
+            # Calculate performance score
+            performance_score = employee.calculate_performance()
+
+            # Insert employee data into the database
+            insert_employee_data(employee)
+
+            # Display results
+            st.subheader(f"{employee_name}'s Performance Score: {performance_score:.2f}")
+
+            # Display additional information
+            st.write(f"\nFeedback: {feedback}")
+            st.write(f"Goals achieved: {goals_achieved}")
+            st.write(f"Self-assessment score: {self_assessment}")
+            st.write(f"Leave taken (days): {leave}")
 
 if __name__ == "__main__":
     main()
+
+# Close the database connection
+cursor.close()
+conn.close()
